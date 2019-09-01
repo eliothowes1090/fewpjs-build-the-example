5c8ada55dbc6cae64b2dd34c5a53bae0b3d1f29d
eliothowes1090 <ejhowes@gmail.com>
2019-09-01 12:58:37 +0100

    Completed Lab

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const likeButtons = document.querySelectorAll('.like-glyph')
+const errorModal = document.querySelector('#modal.hidden')
+const errorMessage = document.querySelector('#modal-message')
 // Your JavaScript code goes here!
 
+const likeEvent = () => {
+  likeButtons.forEach(likeButton => {
+    likeButton.addEventListener('click', event => {
+      dealWithSeverResponse(mimicServerCall(), event)
+    })
+  })
+}
 
+const likeAction = event => {
+  if (event.target.innerText === EMPTY_HEART) {
+    event.target.innerText = FULL_HEART
+    event.target.classList.add('activated-heart')
+  } else if (event.target.innerText === FULL_HEART) {
+    event.target.innerText = EMPTY_HEART
+    event.target.classList.remove('activated-heart')
+  }
+}
+
+const dealWithSeverResponse = (response, event) => {
+  response.then(() => {
+    likeAction(event)
+  }).catch(resp => {
+    errorMessage.innerText = resp
+    errorModal.classList.remove('hidden')
+    setTimeout(() => {errorModal.classList.add('hidden')}, 5000)
+  })
+}
+
+
+
+
+
+
+
+likeEvent()
 
 
 //------------------------------------------------------------------------------
